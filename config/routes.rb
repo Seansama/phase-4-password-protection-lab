6cce1805d5445c91cd6f50fc378093b820316165
Shaun <seansama@pop-os.localdomain>
2023-03-25 21:57:21 +0300

    Lab complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  post '/signup' => 'users#create'
+  get '/me' => 'users#show'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
